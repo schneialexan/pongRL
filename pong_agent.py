dc8308a5f05b2f065011e8a12a9e4bb02fed2fed
Alex Schneider <126069283+schneialexan@users.noreply.github.com>
2023-10-04 13:34:02 +0200

    added working algorithms

--- pong_agent.py
+++ pong_agent.py
@@ -1,5 +1,29 @@
 import gymnasium as gym
+import matplotlib.pyplot as plt
 
-env = gym.make('ALE/Pong-ram-v5', render_mode='rgb_array')
-env.reset()
-print(env.render())
+SEED = 42
+
+env = gym.make('ALE/Pong-v5', render_mode='rgb_array')
+observation, info = env.reset(seed=SEED)
+
+
+# Create a figure for plotting
+plt.figure()
+
+for _ in range(1000):
+    action = env.action_space.sample()  # this is where you would insert your policy
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    # Render the current frame
+    plt.imshow(observation)
+
+    # You can add additional information to the plot if needed, e.g., the reward
+    plt.title(f'Reward: {reward}')
+
+    # Display the frame
+    plt.pause(0.01)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
